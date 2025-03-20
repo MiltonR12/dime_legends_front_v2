809b8ed00011ec144f6897d727880e1d42689d2f
MiltonR12 <miltonaguilar142018@gmail.com>
2025-03-20 18:55:38 -0400

    agregar cambios

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 // export const URL_API = 'http://localhost:3000/api'
-export const URL_API = 'https://api.dimelegends.lat/api'
+// export const URL_API = 'https://api.dimelegends.lat/api'
+export const URL_API = 'https://dime-legends-back-v2.onrender.com'
+
 
 export const GOOGLE_CLIENT_ID = "835575957555-eoa5o8tpmbnn1hr4ce7ngr6ii57oedvi.apps.googleusercontent.com"
