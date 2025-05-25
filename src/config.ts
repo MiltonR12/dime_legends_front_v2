14e703158094115bd925bc9645c82bd5c636436d
MiltonR12 <miltonaguilar142018@gmail.co>
2025-05-25 12:19:54 -0400

    arreglo de la url de prod

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // export const URL_API = 'http://localhost:3000/api'
-export const URL_API = 'httwps://dime-legends-back-v2.onrender.com/api'
+export const URL_API = 'https://dime-legends-back-v2.onrender.com/api'
 
 
 export const GOOGLE_CLIENT_ID = "275723721575-spk4s93td11vp979tdsg026hp9p5jlo7.apps.googleusercontent.com"
